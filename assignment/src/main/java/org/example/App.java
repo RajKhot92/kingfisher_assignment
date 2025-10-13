016981335b2710086b6f132dcd29ad488764f52f
Raj Khot <khot.raj92@gmail.com>
2025-10-13 15:38:48 +0000

    Added code to initialize SKU Unit Price and Promotion.

--- assignment/src/main/java/org/example/App.java
+++ assignment/src/main/java/org/example/App.java
@@ -1,13 +1,17 @@
 package org.example;
 
+import com.assignment.PromotionEngine;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+
+        System.out.println( "Promotion Engine Started!" );
+        PromotionEngine engine = new PromotionEngine();
+        engine.initialize();
     }
 }
